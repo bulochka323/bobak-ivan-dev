055282d37061c6bd2cfe3cc242b0cd88ef81836a
bulochka323 <bulka9605@gmail.com>
2026-09-24 11:37:36 +0300

    Remove About and Experience components
    Removed About and Experience components from the App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
-import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Experience } from "./components/Experience";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
@@ -13,10 +11,8 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
-        <About />
         <Skills />
         <Projects />
-        <Experience />
         <Contact />
       </main>
       <Footer />
