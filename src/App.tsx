49057e2dc293aadabe3d84ce15963ffb3b91269d
bulochka323 <bulka9605@gmail.com>
2026-09-24 12:15:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <About />
         <Skills />
         <Projects />
         <Contact />
